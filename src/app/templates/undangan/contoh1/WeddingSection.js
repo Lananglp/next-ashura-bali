02cfee61be727f77d9facd4f9c51ba3b335f6592
lanang <lananglanusaputera@gmail.com>
2024-06-03 12:37:29 +0800

    lanang

--- src/app/templates/undangan/contoh1/WeddingSection.js
+++ src/app/templates/undangan/contoh1/WeddingSection.js
@@ -17,7 +17,7 @@ const birthstone = Birthstone({
 
 function OpenWedding({images}) {
 
-    const [open, setOpen] = useState(false); // ubah jadi false
+    const [open, setOpen] = useState(true); // ubah jadi false
     // const audioRef = useRef(new Audio('/audio/soundWedding.mp3'));
     const [music, setMusic] = useState(false);
 
@@ -50,7 +50,7 @@ function OpenWedding({images}) {
 
     return (
         <>
-            <AnimatePresence>
+            {/* <AnimatePresence>
                 {!open &&
                     <motion.div 
                         className='h-screen bg-black'
@@ -81,7 +81,7 @@ function OpenWedding({images}) {
                         </div>
                     </motion.div>
                 }
-            </AnimatePresence>
+            </AnimatePresence> */}
             <AnimatePresence>
                 {open && 
                     <motion.div
@@ -91,34 +91,42 @@ function OpenWedding({images}) {
                     >
                         <div className='relative'>
                             <div className='absolute inset-0 bg-gradient-to-t from-transparent to-black/50 from-[70%] to-[100%]'/>
-                            <div className='absolute inset-x-0 top-2'>
-                                <Image src='/image/decoration-1.svg' alt='Image1' width={0} height={0} className='mx-auto w-56 h-auto'/>
+                            <Image src='/image/border-t-l-2.svg' alt='Image1' width={0} height={0} className='w-32 h-auto absolute top-0 start-0'/>
+                            <Image src='/image/border-t-r-2.svg' alt='Image1' width={0} height={0} className='w-32 h-auto absolute top-0 end-0'/>
+                            <div className='absolute inset-x-0 top-6'>
+                                <Image src='/image/decoration-1.svg' alt='Image1' width={0} height={0} className='mx-auto w-32 h-auto opacity-75'/>
                             </div>
                             <Image priority src={images[0]} alt='Image1' width={354} height={512} className='w-full h-[512px] object-cover'/>
                             <div className='absolute inset-0 bg-gradient-to-b from-transparent to-black from-[50%] to-[100%]'/>
+                            <Image src='/image/border-b-l-2.svg' alt='Image1' width={0} height={0} className='w-16 h-16 opacity-75 absolute bottom-8 start-2'/>
+                            <Image src='/image/border-b-r-2.svg' alt='Image1' width={0} height={0} className='w-16 h-16 opacity-75 absolute bottom-8 end-2'/>
                         </div>
 
                         <div className='relative px-4 py-12'>
-                            <Image src='/image/flower-l.svg' alt='Image1' width={0} height={0} className='w-24 h-24 opacity-50 absolute -top-12 start-0'/>
-                            <Image src='/image/flower-r.svg' alt='Image1' width={0} height={0} className='w-24 h-24 opacity-50 absolute top-20 end-0'/>
+                            <Image src='/image/flower-l-2.svg' alt='Image1' width={0} height={0} className='w-36 h-36 opacity-75 absolute bottom-0 start-0'/>
+                            <Image src='/image/flower-r-2.svg' alt='Image1' width={0} height={0} className='w-36 h-36 opacity-75 absolute bottom-0 end-0'/>
+                            {/* <Image src='/image/flower-l.svg' alt='Image1' width={0} height={0} className='w-24 h-24 opacity-75 absolute -top-12 start-0'/>
+                            <Image src='/image/flower-r.svg' alt='Image1' width={0} height={0} className='w-24 h-24 opacity-75 absolute top-20 end-0'/> */}
                             <div className='absolute inset-x-0 -top-16'>
                                 <p className='mb-2 tracking-wide text-center text-yellow-100 font-light'>Pawiwahan</p>
                                 <h1 className={`${birthstone.className} text-center mb-4 text-5xl text-yellow-200`}>Dwika &amp; Erina</h1>
                             </div>
                             <p className='px-8 italic tracking-wide text-center text-yellow-100 font-light text-sm'>&quot; Ihaiva stam mā vi yaustam, Visvām āyur vyasnutam. Krindantau putrair naptrbhih, Modamānau sve grhe. &quot;</p>
                             <div className='my-6 mx-auto w-56 border-b border-zinc-800' />
-                            <p className='px-4 tracking-wide text-center font-light text-sm'>Wahai pasangan suami-isteri, semoga kalian tetap bersatu dan tidak pernah terpisahkan. Semoga kalian mencapai hidup penuh kebahagiaan, tinggal di rumah yang penuh kegembiraan bersama seluruh keturunanmu.</p>
+                            <p className='mb-4 px-4 tracking-wide text-center font-light text-sm'>Wahai pasangan suami-isteri, semoga kalian tetap bersatu dan tidak pernah terpisahkan. Semoga kalian mencapai hidup penuh kebahagiaan, tinggal di rumah yang penuh kegembiraan bersama seluruh keturunanmu.</p>
                         </div>
 
                         <div className='relative'>
+                            <Image src='/image/border-b-l-2.svg' alt='Image1' width={0} height={0} className='w-20 h-20 opacity-75 absolute top-2 end-2 rotate-180'/>
+                            <Image src='/image/border-b-r-2.svg' alt='Image1' width={0} height={0} className='w-20 h-20 opacity-75 absolute top-2 start-2 rotate-180'/>
                             <div className='absolute inset-0 bg-gradient-to-t from-transparent to-black/50 from-[95%] to-[100%]'/>
                             <Image src='/image/undangan/man.jpeg' alt='Image1' width={354} height={512} className='w-full h-[512px] object-cover'/>
                             <div className='absolute inset-0 bg-gradient-to-b from-transparent to-black from-[50%] to-[100%]'/>
                         </div>
 
                         <div className='relative px-4 py-12'>
-                            <Image src='/image/flower-l-2.svg' alt='Image1' width={154} height={0} className='w-auto h-auto opacity-30 absolute bottom-0 start-0'/>
-                            <Image src='/image/flower-r-2.svg' alt='Image1' width={154} height={0} className='w-auto h-auto opacity-30 absolute bottom-0 end-0'/>
+                            <Image src='/image/flower-l-2.svg' alt='Image1' width={0} height={0} className='w-36 h-36 opacity-75 absolute bottom-0 start-0'/>
+                            <Image src='/image/flower-r-2.svg' alt='Image1' width={0} height={0} className='w-36 h-36 opacity-75 absolute bottom-0 end-0'/>
                             <div className='absolute inset-x-0 -top-16'>
                                 <h2 className={`${birthstone.className} text-center mb-2 text-5xl text-yellow-200`}>Bro Dwika</h2>
                                 <p className='tracking-wide text-center text-yellow-100 font-light'>I Made Dwika Ananta Gunawan, S.kom</p>
@@ -127,18 +135,20 @@ function OpenWedding({images}) {
                             <p className='mb-2 tracking-wide text-center text-yellow-100 font-light'>I Wayan Gredeg Jaya</p>
                             <p className='mb-2 tracking-wide text-center font-light text-4xl'>&amp;</p>
                             <p className='mb-2 tracking-wide text-center text-yellow-100 font-light'>Ni Ketut Laravel Putriyani</p>
-                            <p className='px-4 tracking-wide text-center font-light text-sm'>Br.Gulingan, Antosari, Selemadeg Barat, Tabanan, Bali</p>
+                            <p className='mb-4 px-12 tracking-wide text-center font-light text-sm'>Br.Gulingan, Antosari, Selemadeg Barat, Tabanan, Bali</p>
                         </div>
 
                         <div className='relative'>
+                            <Image src='/image/border-b-l-2.svg' alt='Image1' width={0} height={0} className='w-20 h-20 opacity-100 absolute top-2 end-2 rotate-180'/>
+                            <Image src='/image/border-b-r-2.svg' alt='Image1' width={0} height={0} className='w-20 h-20 opacity-100 absolute top-2 start-2 rotate-180'/>
                             <div className='absolute inset-0 bg-gradient-to-t from-transparent to-black/50 from-[95%] to-[100%]'/>
                             <Image src='/image/undangan/woman.jpeg' alt='Image1' width={354} height={512} className='w-full h-[512px] object-cover'/>
                             <div className='absolute inset-0 bg-gradient-to-b from-transparent to-black from-[50%] to-[100%]'/>
                         </div>
 
                         <div className='relative px-4 py-12'>
-                            <Image src='/image/flower-l-2.svg' alt='Image1' width={154} height={0} className='w-auto h-auto opacity-30 absolute bottom-0 start-0'/>
-                            <Image src='/image/flower-r-2.svg' alt='Image1' width={154} height={0} className='w-auto h-auto opacity-30 absolute bottom-0 end-0'/>
+                            <Image src='/image/flower-l-2.svg' alt='Image1' width={0} height={0} className='w-36 h-36 opacity-75 absolute bottom-0 start-0'/>
+                            <Image src='/image/flower-r-2.svg' alt='Image1' width={0} height={0} className='w-36 h-36 opacity-75 absolute bottom-0 end-0'/>
                             <div className='absolute inset-x-0 -top-16'>
                                 <h2 className={`${birthstone.className} text-center mb-2 text-5xl text-yellow-200`}>Mbak Erina</h2>
                                 <p className='tracking-wide text-center text-yellow-100 font-light'>Ni Made Erina Cipta Yanti</p>
@@ -147,26 +157,30 @@ function OpenWedding({images}) {
                             <p className='mb-2 tracking-wide text-center text-yellow-100 font-light'>I Putu Tigtig putra</p>
                             <p className='mb-2 tracking-wide text-center font-light text-4xl'>&amp;</p>
                             <p className='mb-2 tracking-wide text-center text-yellow-100 font-light'>Ni Luh Syanti Sekar</p>
-                            <p className='px-4 tracking-wide text-center font-light text-sm'>Br.Gulingan, Antosari, Selemadeg Barat, Tabanan, Bali</p>
+                            <p className='mb-4 px-12 tracking-wide text-center font-light text-sm'>Br.Gulingan, Antosari, Selemadeg Barat, Tabanan, Bali</p>
                         </div>
 
                         <div className='relative'>
+                            <Image src='/image/border-b-l-2.svg' alt='Image1' width={0} height={0} className='w-20 h-20 opacity-100 absolute top-2 end-2 rotate-180'/>
+                            <Image src='/image/border-b-r-2.svg' alt='Image1' width={0} height={0} className='w-20 h-20 opacity-100 absolute top-2 start-2 rotate-180'/>
                             <div className='absolute inset-0 bg-gradient-to-t from-transparent to-black/50 from-[95%] to-[100%]'/>
                             <Image src={images[2]} alt='Image1' width={354} height={512} className='w-full h-[512px] object-cover'/>
                             <div className='absolute inset-0 bg-gradient-to-b from-transparent to-black from-[50%] to-[100%]'/>
                         </div>
 
                         <div className='relative px-4 py-12'>
-                            <Image src='/image/flower-l-2.svg' alt='Image1' width={154} height={0} className='w-auto h-auto opacity-30 absolute bottom-0 start-0'/>
-                            <Image src='/image/flower-r-2.svg' alt='Image1' width={154} height={0} className='w-auto h-auto opacity-30 absolute bottom-0 end-0'/>
+                            <Image src='/image/flower-l-2.svg' alt='Image1' width={0} height={0} className='w-36 h-36 opacity-75 absolute bottom-0 start-0'/>
+                            <Image src='/image/flower-r-2.svg' alt='Image1' width={0} height={0} className='w-36 h-36 opacity-75 absolute bottom-0 end-0'/>
                             <div className='absolute inset-x-0 -top-16'>
                                 <p className='mb-2 tracking-wide text-center text-yellow-100 font-light'>Waktu dan Lokasi</p>
                                 <h1 className={`${birthstone.className} text-center mb-4 text-5xl text-yellow-200`}>Acara Kami</h1>
                             </div>
                             <p className='mb-6 px-4 tracking-wide text-center font-light text-lg'>17:00 s/d Selesai</p>
                             <div className='my-6 mx-auto w-56 border-b border-zinc-800' />
-                            <p className='mb-6 px-4 tracking-wide text-center font-light'>Br. Gulingan, Antosari, Selemadeg Barat, Tabanan, Bali</p>
-                            <Link href='/' className='block px-4 py-2 border border-yellow-200 text-yellow-200 text-center'><FaMapMarkerAlt className='inline me-1 mb-0.5'/> Buka Google Map</Link>
+                            <p className='mb-6 px-8 tracking-wide text-center font-light'>Br. Gulingan, Antosari, Selemadeg Barat, Tabanan, Bali</p>
+                            <div className='mb-6 text-center'>
+                                <Link href='/' className='inline-block px-4 py-2 border border-yellow-200 text-yellow-200 text-center'><FaMapMarkerAlt className='inline me-1 mb-0.5'/> Buka Google Map</Link>
+                            </div>
                             {/* <p className='px-8 italic tracking-wide text-center text-yellow-100 font-light text-sm'>&quot; Ihaiva stam mā vi yaustam, Visvām āyur vyasnutam. Krindantau putrair naptrbhih, Modamānau sve grhe. &quot;</p> */}
                         </div>
 
